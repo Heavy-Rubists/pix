278dd659f2ae722171a4203d3bc54baeb7b2020a
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-11-05 03:32:45 +0300

    added routing
    - added registration controller
    - structured html pages into different folders

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,21 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  
   get "up" => "rails/health#show", as: :rails_health_check
-  root "application#account"
+  root "registration#new"
   # Defines the root path route ("/")
   #root "posts#index"
+
+  resources :registration, only: [:new, :create]
+  resources :home, only: [:new]
+  
+  # or it can be
+  # get "/registration", to: "registration#new"
+  # post "/registration", to: "registration#create"
+  # 
+  # get "/login", to: "login#new"
+  # get "/login", to: "login#create"
+  # get "/login", to: "login#delete"
 end
