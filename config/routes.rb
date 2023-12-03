75099650ac56a7b950807f9e996125e62eb254b1
Владислав Крылов <krylov@sfedu.ru>
2023-12-03 21:09:35 +0300

    Implemented adding photos to ActiveStorage
    Problem: no user id provided

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
 
-  
+
   get "up" => "rails/health#show", as: :rails_health_check
   root "registration#new"
   # Defines the root path route ("/")
@@ -12,15 +12,16 @@ Rails.application.routes.draw do
 
   resources :registration, only: [:new, :create]
   resources :home, only: [:index]
-  
+
   # or it can be
   # get "/registration", to: "registration#new"
   # post "/registration", to: "registration#create"
-  # 
+  #
   get "/login", to: "login#new"
   post "/login", to: "login#create"
   delete "logout", to: "login#destroy"
   get "/feed", to: "feed#new"
   get "/registration", to: "registration#new"
   get "/upload", to: "upload#new"
+  post "/upload", to: "upload#create"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_16_213504) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_03_173654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,10 +48,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_213504) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content", null: false
+    t.bigint "photo_id", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "photo_id", null: false
     t.index ["photo_id"], name: "index_comments_on_photo_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -75,8 +75,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_213504) do
 
   create_table "photos", force: :cascade do |t|
     t.text "description"
-    t.text "as_url", null: false
-    t.date "upload_date", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
