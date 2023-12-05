2eb20b4e56b880d2a7bb94088daf0279628fee63
PD_Creator <anuchin@sfedu.ru>
2023-12-05 21:55:26 +0300

    added demo validation, fixed bug
    bug: "style.css is not presented in pipeline"

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,13 @@
 class Photo < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  # validates :user_id, presence: true, format: { with: / \.(jpg|jpeg|png|gif|apng|tiff|tif|bmp|xcf|webp")$ /i, message: "custom message" }
+  # validate :correct_image_type
+  # private 
+  # def correct_image_type
+  #   if image.attached? && !image.content_type.in?(%w(image/jpeg image/png))
+  #     errors.add(:image, "Must be jpeg or png")
+  #   elsif image.attached? == false
+  #     errors.add(:image, "must have an image attached")
+  #   end
+  # end
 end
