e4fa2c195743fc689bf0bc00400fd5546b1d6f64
PD_Creator <anuchin@sfedu.ru>
2023-12-05 21:32:10 +0300

    implemented descriptions to pictures
    added style to pictures in home page, now they are posts

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  # validates :user_id, presence: true, format: { with: / \.(jpg|jpeg|png|gif|apng|tiff|tif|bmp|xcf|webp")$ /i, message: "custom message" }
 end
