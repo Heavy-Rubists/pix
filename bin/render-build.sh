71017ad38123e5789d8cd85648f0ae053e3ff3e5
TheBogdichHD <nottoowellconstructed@gmail.com>
2023-12-11 09:27:20 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+
 bundle install
+npm install
+npm run build
+npm run build:css
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
